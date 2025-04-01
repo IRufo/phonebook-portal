20553773a243f17da0ff376d2a585db3c6f35600
IRufo <irishs.rufo@gmail.com>
2025-04-01 17:52:04 +0800

    remove logout button

--- src/pages/auth/AccountStatus.tsx
+++ src/pages/auth/AccountStatus.tsx
@@ -1,6 +1,5 @@
 import { Box, VStack, Heading, Text, IconButton, Icon, Group, Link, Center } from "@chakra-ui/react";
-import { SlLogout, SlInfo  } from "react-icons/sl";
-import { Tooltip } from "../../components/ui/tooltip"
+import { SlInfo  } from "react-icons/sl";
 
 const AccountStatus = () => {
   return (
@@ -15,15 +14,6 @@ const AccountStatus = () => {
             <VStack>
                 <Text textStyle="sm">Account created successfully! Access will be granted upon admin approval. Please wait. Thank you.<b>See you!</b></Text>
             </VStack>
-            <br/>   
-            <Group>
-                <Tooltip content="logout for now" >
-                    <IconButton variant="subtle" rounded="full">
-                        <SlLogout />
-                    </IconButton>
-                </Tooltip>
-                <Link href="#"><Text textStyle="sm">Logout</Text></Link>
-            </Group>
         </Box>
     </Box>
   );
