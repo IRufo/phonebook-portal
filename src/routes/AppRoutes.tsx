03f73294045679289d8e48dc8cbd4c936c799ad0
IRufo <irishs.rufo@gmail.com>
2025-04-01 15:51:31 +0800

    account status

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,7 +2,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
-// import AccountStatus from "../components/auth/AccountStatus";
+import AccountStatus from "../components/auth/AccountStatus";
 
 
 const AppRoutes = () => {
@@ -10,7 +10,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      {/* <Route path="/account-status" element={<AccountStatus />} /> */}
+      <Route path="/account-status" element={<AccountStatus />} />
       {/* <Route path="/profile" element={<ProfilePage />} /> */}
     </Routes>
   );
